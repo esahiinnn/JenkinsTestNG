8dac51ac71d9ff7d6df82960f3f3d23614813ab5
Ebubekir <esahin3@icloud.com>
2021-05-09 19:58:41 +0300

    Test

---
+++ src/test/java/tests/Test2.java
@@ -0,0 +1,18 @@
+package tests;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class Test2 {
+
+    @Test
+    public void test(){
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://google.com");
+        String expectedTitle = "Google";
+        Assert.assertEquals(driver.getTitle(), expectedTitle);
+
+    }
+}
