94deccd5f758da42643a6770de390f7234fc9b10
Ebubekir <esahin3@icloud.com>
2021-05-09 21:51:54 +0300

    we

--- src/test/java/runners/Runner.java
+++ src/test/java/runners/Runner.java
@@ -7,7 +7,7 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         plugin = {
-                "html:target/default-cucumber-reports3",
+                "html:target/default-cucumber-reports3.html",
                 "json:target/json-report/cucumber3.json",
                 "junit:target/xml-report/cucumber3.xml"
         },
