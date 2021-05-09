f1da51100555fd77ef020a13296280ff5146bb10
Ebubekir <esahin3@icloud.com>
2021-05-09 21:37:09 +0300

    w

--- src/test/java/tests/Test2.java
+++ src/test/java/tests/Test2.java
@@ -3,11 +3,14 @@ package tests;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.rmi.Remote;
+
 public class Test2 {
-    WebDriver driver;
+    RemoteWebDriver driver;
     @Test
     public void test(){
         WebDriverManager.chromedriver().setup();
