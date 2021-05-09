614820976af1e3e57fb20e45c644575f08f27753
Ebubekir <esahin3@icloud.com>
2021-05-09 22:38:50 +0300

    j

---
+++ target/default-cucumber-reports3.html/report.js
@@ -0,0 +1,204 @@
+$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("file:src/test/resources/features/a.feature");
+formatter.feature({
+  "name": "user should find the related products on google search",
+  "description": "",
+  "keyword": "Feature",
+  "tags": [
+    {
+      "name": "@googlesearch"
+    }
+  ]
+});
+formatter.background({
+  "name": "",
+  "description": "",
+  "keyword": "Background"
+});
+formatter.step({
+  "name": "user is on the google page",
+  "keyword": "Given "
+});
+formatter.match({
+  "location": "stepdefinitions.AStepDefinitions.user_is_on_the_google_page()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.scenario({
+  "name": "TC_01_iphone search",
+  "description": "",
+  "keyword": "Scenario",
+  "tags": [
+    {
+      "name": "@googlesearch"
+    },
+    {
+      "name": "@iphone"
+    }
+  ]
+});
+formatter.step({
+  "name": "user search for iPhone",
+  "keyword": "When "
+});
+formatter.match({
+  "location": "stepdefinitions.AStepDefinitions.user_search_for_iPhone()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "verify the result has iPhone",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "stepdefinitions.AStepDefinitions.verify_the_result_has_iPhone()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.background({
+  "name": "",
+  "description": "",
+  "keyword": "Background"
+});
+formatter.step({
+  "name": "user is on the google page",
+  "keyword": "Given "
+});
+formatter.match({
+  "location": "stepdefinitions.AStepDefinitions.user_is_on_the_google_page()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.scenario({
+  "name": "TC_02_tea pot search",
+  "description": "",
+  "keyword": "Scenario",
+  "tags": [
+    {
+      "name": "@googlesearch"
+    }
+  ]
+});
+formatter.step({
+  "name": "user search for tea pot",
+  "keyword": "When "
+});
+formatter.match({
+  "location": "stepdefinitions.AStepDefinitions.user_search_for_tea_pot()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "verify the result has tea pot",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "stepdefinitions.AStepDefinitions.verify_the_result_has_tea_pot()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.background({
+  "name": "",
+  "description": "",
+  "keyword": "Background"
+});
+formatter.step({
+  "name": "user is on the google page",
+  "keyword": "Given "
+});
+formatter.match({
+  "location": "stepdefinitions.AStepDefinitions.user_is_on_the_google_page()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.scenario({
+  "name": "TC_03_flower search",
+  "description": "",
+  "keyword": "Scenario",
+  "tags": [
+    {
+      "name": "@googlesearch"
+    },
+    {
+      "name": "@flower"
+    }
+  ]
+});
+formatter.step({
+  "name": "user search for flower",
+  "keyword": "When "
+});
+formatter.match({
+  "location": "stepdefinitions.AStepDefinitions.user_search_for_flower()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "verify the result has flower",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "stepdefinitions.AStepDefinitions.verify_the_result_has_flower()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.background({
+  "name": "",
+  "description": "",
+  "keyword": "Background"
+});
+formatter.step({
+  "name": "user is on the google page",
+  "keyword": "Given "
+});
+formatter.match({
+  "location": "stepdefinitions.AStepDefinitions.user_is_on_the_google_page()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.scenario({
+  "name": "TC_04_bmw search",
+  "description": "",
+  "keyword": "Scenario",
+  "tags": [
+    {
+      "name": "@googlesearch"
+    },
+    {
+      "name": "@iphone"
+    },
+    {
+      "name": "@flower"
+    }
+  ]
+});
+formatter.step({
+  "name": "user search for bmw",
+  "keyword": "When "
+});
+formatter.match({
+  "location": "stepdefinitions.AStepDefinitions.user_search_for_bmw()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "verify the result bmw",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "stepdefinitions.AStepDefinitions.verify_the_result_bmw()"
+});
+formatter.result({
+  "status": "passed"
+});
+});
